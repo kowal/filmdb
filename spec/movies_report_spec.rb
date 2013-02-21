6e35d94ea714e0f97815c5182f9eb3a34b8b6cda
Marek Kowalcze <marek.kowalcze@gmail.com>
2013-02-21 23:19:37 +0100

    Move expected movies collection to *.yml file

--- spec/movies_report_spec.rb
+++ spec/movies_report_spec.rb
@@ -2,44 +2,19 @@
 
 require 'spec_helper'
 
+# To update fixtures/expected/*.yml, run following JS in browser:
+#
+#     $.map($('PATTERN'), function(el, idx) { return $(el).text().trim() })
+#
 describe MoviesReport do
   context 'run on chomikuj page' do
     it "should find movies included in the page" do
       VCR.use_cassette('chomikuj') do
-        movies_collection = MoviesReport::DSL.parse_html 'http://chomikuj.pl/SHREC'
-        expected = <<-DOC
-Lot
-OCZY SMOKA
-Straznicy marzen
-Miami Vice
-Mama
-The Baytown Outlaws
-Bez hamulców
-Zjawy
-Zemsta Po Śmierci
-SKYFALL
-Nędznicy
-Dziewczyna z Sushi
-Savages - Poza Bezprawiem
-Ralph Demolka
-6 kul - Six Bullets
-Step Up 3D
-Akademia Wojskowa
-11-11-11
-Cichy dom
-The Tall Man
-Bucky Larson. Urodzony gwiazdor
-Żona na niby
-Nawiedzona narzeczona
-Love guru
-Młody Einstein
-Łatwa Dziewczyna
-Poznaj moich spartan
-Stare Wygi
-Święto piwa
-DOC
-        expected.split("\n").each do |title|
-          expect(movies_collection).to include(title)
+        actual_movies = MoviesReport::DSL.parse_html 'http://chomikuj.pl/SHREC'
+        expected_movies = YAML::load(File.open('fixtures/expected/chomikuj.yml'))
+
+        expected_movies.each do |title|
+          expect(actual_movies).to include(title)
         end
       end
     end
