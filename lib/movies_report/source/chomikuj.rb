827068a9cbef74aa9bc7a2ecefdb84f649479876
Marek Kowalcze <marek.kowalcze@gmail.com>
2013-05-28 23:50:25 +0200

    Code cleanup via rubocup suggestions

--- lib/movies_report/source/chomikuj.rb
+++ lib/movies_report/source/chomikuj.rb
@@ -42,28 +42,36 @@ module MoviesReport
 
       def predefined_page_info(page_name)
         {
-          folder_list: {
-            selector: '#foldersList a',
-            fields: {
-              title: ->(el) { sanitize_title(el.content.strip) }
-            }
-          },
-          file_list: {
-            selector: '#FilesListContainer .fileItemContainer',
-            fields: {
-              title: ->(el) do
-                sanitize_title(el.css('.filename').first.content.strip)
-              end,
-              size:  ->(el) do
-                el.css('.fileinfo li:nth-last-child(2)').first.content
-              end
-            }
-          }
+          folder_list: folder_list_info,
+          file_list: file_list_info
         }[page_name]
+      end
+
+      def folder_list_info
+        {
+          selector: '#foldersList a',
+          fields: {
+            title: ->(el) { sanitize_title(el.content.strip) }
+          }
+        }
+      end
+
+      def file_list_info
+        {
+          selector: '#FilesListContainer .fileItemContainer',
+          fields: {
+            title: ->(el) { sanitize_title(find_first(el, '.filename').strip) },
+            size:  ->(el) { find_first(el, '.fileinfo li:nth-last-child(2)') }
+          }
+        }
       end
 
       def sanitize_title(original_title)
         ChomikujSanitizer.clean(original_title)
+      end
+
+      def find_first(element, selector)
+        element.css(selector).first.content
       end
 
     end
@@ -71,12 +79,17 @@ module MoviesReport
     class ChomikujSanitizer
 
       TO_REMOVE = %w{
-        .BRRiP MX PL.DVDRiP.XviD.AC3-PBWT PL.DVDRiP.XViD-J25 DVDRip.XviD-LEKTOR HDRip.XviD-LEKTOR PL.PDTV.XViD-Zet
-        PLSUBBED.BRRip.XviD-BiDA DVDRiP DVDRip XViD PSiG XviD.AC3-PBWT XviD AC3-PBWT 480p AC3-MORS PL.DVDRiP.X
-        XviD.Zet XviD-max184 XviD-Zet .-Zet XviD-BiDA XviD PDTV .ViD-aX HDRip BDrip PL.DVDRip.XviD-max184 -max184
-        BDRip-max184 .BDRip.-BiDA .BRRip-BiDA .BRRip .BRRip.XviD .BDRip.XviD -GHW -BiDA .-BiDA DRip-BiDA .HDTV TVRip
-        lektor Lektor lekyor .napisy SUBBED.B SUBBED -LEKTOR 2012.PLDUB PL.DVDRip.XviD-Zet
-        -orgonalny --orgonalny --orgoinalny .oryginalny oryginalny --oryginalny --orginalny orginalny
+        .BRRiP MX PL.DVDRiP.XviD.AC3-PBWT PL.DVDRiP.XViD-J25
+        DVDRip.XviD-LEKTOR HDRip.XviD-LEKTOR PL.PDTV.XViD-Zet -max184
+        PLSUBBED.BRRip.XviD-BiDA DVDRiP DVDRip XViD PSiG XviD.AC3-PBWT
+        XviD AC3-PBWT 480p AC3-MORS PL.DVDRiP.X XviD.Zet XviD-max184 XviD-Zet
+        .-Zet XviD-BiDA XviD PDTV .ViD-aX HDRip BDrip PL.DVDRip.XviD-max184
+        BDRip-max184 .BDRip.-BiDA .BRRip-BiDA .BRRip .BRRip.XviD .BDRip.XviD
+        -GHW -BiDA .-BiDA DRip-BiDA .HDTV TVRip
+        lektor Lektor lekyor .napisy SUBBED.B SUBBED -LEKTOR 2012.PLDUB
+        PL.DVDRip.XviD-Zet
+        -orgonalny --orgonalny --orgoinalny .oryginalny oryginalny
+        --oryginalny --orginalny orginalny
         .pl PL IVO(1).avi ivo IVO Ivo 2012.PL dubbing.pl.avi Dubbing
         chomikuj Chomikuj.avi .avi -- .--cam cam.avi ==
       }
