d996489e8899bb62c8fcf0ed8407aef528dc1040
Marek Kowalcze <marek.kowalcze@gmail.com>
2013-05-28 23:43:47 +0200

    Code cleanup via rubocup suggestions: Prefer single-quoted strings

--- lib/movies_report.rb
+++ lib/movies_report.rb
@@ -1,26 +1,26 @@
 # coding: utf-8
 
 require 'nokogiri'
-require "net/http"
-require "uri"
-require "awesome_print"
-require "imdb"
+require 'net/http'
+require 'uri'
+require 'awesome_print'
+require 'imdb'
 
 module MoviesReport
 
-  require "movies_report/version"
-  require "movies_report/config"
+  require 'movies_report/version'
+  require 'movies_report/config'
 
-  require "movies_report/html_page"
-  require "movies_report/search/base_search"
-  require "movies_report/search/filmweb"
-  require "movies_report/search/imdb"
-  require "movies_report/source/chomikuj"
+  require 'movies_report/html_page'
+  require 'movies_report/search/base_search'
+  require 'movies_report/search/filmweb'
+  require 'movies_report/search/imdb'
+  require 'movies_report/source/chomikuj'
 
-  require "movies_report/report"
+  require 'movies_report/report'
 
-  require "movies_report/dsl"
-  require "movies_report/cli"
+  require 'movies_report/dsl'
+  require 'movies_report/cli'
 end
 
 MoviesReport.configure do |config|
