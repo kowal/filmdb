ea31564943a5ff51196422f2dd4ea067e018aef6
Marek Kowalcze <marek.kowalcze@gmail.com>
2013-08-29 22:27:13 +0200

    Formatting in filmweb_spec

--- spec/search/filmweb_spec.rb
+++ spec/search/filmweb_spec.rb
@@ -14,24 +14,30 @@ describe MoviesReport::Search::Filmweb do
   context '#rating' do
 
     it 'returns float for x,x/y format' do
-      fake_service.expects(:find).with(movie_title)
+      fake_service
+        .expects(:find)
+        .with(movie_title)
         .returns({ rating: '7,1/10' })
 
       expect(filmweb_search.rating).to eq(7.1)
     end
 
     it 'returns float for x,x format' do
-      fake_service.expects(:find).with(movie_title)
-        .returns({ rating: '7,1' })
+      fake_service
+        .expects(:find)
+        .with(movie_title)
+        .returns({ rating: '5,5' })
 
-      expect(filmweb_search.rating).to eq(7.1)
+      expect(filmweb_search.rating).to eq(5.5)
     end
 
     it 'returns float for x.x format' do
-      fake_service.expects(:find).with(movie_title)
-        .returns({ rating: '7.1' })
+      fake_service
+        .expects(:find)
+        .with(movie_title)
+        .returns({ rating: '9.9' })
 
-      expect(filmweb_search.rating).to eq(7.1)
+      expect(filmweb_search.rating).to eq(9.9)
     end
 
   end
