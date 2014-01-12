22fc98d6a8c839321f2a49983c0aca9818b3d9b4
Marek Kowalcze <marek.kowalcze@gmail.com>
2014-01-12 15:11:46 +0100

    Refactor Strategy::Background

--- lib/movies_report/strategy/background.rb
+++ lib/movies_report/strategy/background.rb
@@ -20,25 +20,77 @@ module MoviesReport
       # @return [Hash] hash with current results
       #
       def current_result(workers_ids)
-        results = {}
-        stats = { started: 0, finished: 0 }
+        calculate_current_results(workers_ids)
+      end
+
+      # @private
+      def calculate_current_results(workers_ids)
+        results = StatsCollection.new
+
         workers_ids.each do |worker_id|
-          data = MoviesReport::WebSearchWorker.get_worker_data(worker_id)
+          stats = ResultsStats.new(read_worker_data(worker_id))
+          return {} unless stats.valid? # TODO: this should not be required
 
-          return {} unless data['state']
+          results.increment_state_counter(stats['state'])
+          results.add_rating(stats['title'], stats.get_rating)
+        end
 
-          stats[data['state'].to_sym] += 1
+        results.calculate_ratings
+      end
 
-          results[data['title']] ||= []
-          if data['rating'] && data['rating'] != '' && data['rating'] != '0.0'
-            results[data['title']] << Float(data['rating'])
+      def read_worker_data(worker_id)
+        MoviesReport::WebSearchWorker.get_worker_data(worker_id)
+      end
+
+      # @private
+      class StatsCollection
+
+        def initialize
+          @results = {}
+          @hash_results = { status: { started: 0, finished: 0 } }
+        end
+
+        def increment_state_counter(state)
+          @hash_results[:status][state.to_sym] += 1
+        end
+
+        def add_rating(title, rating)
+          @results[title] ||= []
+          if rating
+            @results[title] << rating
           end
         end
-        hash_results = { status: stats }
-        results.map do |title, ratings|
-          hash_results[title] = (ratings.compact.reduce { |sum, el| sum + el }.to_f / ratings.size).round(1)
+
+        def calculate_ratings
+          @results.map do |title, ratings|
+            @hash_results[title] = (ratings.compact.reduce { |sum, el| sum + el }.to_f / ratings.size).round(1)
+          end
+          @hash_results
         end
-        hash_results
+
+      end
+
+      # @private
+      class ResultsStats
+
+        def initialize(data)
+          @data = data
+        end
+
+        def [](attr_name)
+          @data[attr_name]
+        end
+
+        def valid?
+          @data['state'] != nil
+        end
+
+        def get_rating
+          if @data['rating'] && @data['rating'] != '' && @data['rating'] != '0.0'
+            Float(@data['rating'])
+          end
+        end
+
       end
 
     end
