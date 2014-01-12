e795d4ec84e0935475d740347ad8d785a613a055
Marek Kowalcze <marek.kowalcze@gmail.com>
2014-01-12 14:21:34 +0100

    better specs

--- spec/strategy/background_strategy_spec.rb
+++ spec/strategy/background_strategy_spec.rb
@@ -6,29 +6,40 @@ describe MoviesReport::Strategy::Background do
 
   context '#current_result' do
 
-    it 'returns current ratings for each movie' do
-      stub_film_rating 1,  'MovieA', 'finished', 5.0
-      stub_film_rating 2,  'MovieA', 'finished', 10.0
-      stub_film_rating 5,  'MovieA', 'finished', 6.5
-      stub_film_rating 10, 'MovieB', 'finished', 2.0
-      stub_film_rating 20, 'MovieB', 'started',  3.0
-      stub_film_rating 50, 'MovieB', 'started',  10.0
+    before do
+      stub_film_rating 1,  'title' => 'MovieA', 'state' => 'finished', 'rating' => "5.0"
+      stub_film_rating 2,  'title' => 'MovieA', 'state' => 'finished', 'rating' => "10.0"
+      stub_film_rating 5,  'title' => 'MovieA', 'state' => 'finished', 'rating' => "6.5"
+      stub_film_rating 10, 'title' => 'MovieB', 'state' => 'finished', 'rating' => "2.0"
+      stub_film_rating 20, 'title' => 'MovieB', 'state' => 'started', 'rating' => "3.0"
+      stub_film_rating 50, 'title' => 'MovieB', 'state' => 'started', 'rating' => "10.0"
+    end
 
-      expect(subject.current_result([1, 2, 5, 10, 20, 50])).to eq({
-          'MovieA' => 7.2,
-          'MovieB' => 5.0,
-          :status => { :started => 2, :finished => 4 }
-        })
+    it 'returns current average ratings for each movie' do
+      results = subject.current_result([1, 2, 5, 10, 20, 50])
+
+      expect(results).to include({ 'MovieA' => 7.2, 'MovieB' => 5.0 })
+    end
+
+    it 'returns status count per each status' do
+      results = subject.current_result([1, 2, 5, 10, 20, 50])
+
+      expect(results).to include(:status => { :started => 2, :finished => 4 })
+    end
+
+    it 'returns empty hash when state is blank' do
+      stub_film_rating 100, { 'state' => nil }
+
+      expect(subject.current_result([100])).to eq({ })
     end
 
   end
 
-  def stub_film_rating(worker_id, title, state, rating)
-    MoviesReport::WebSearchWorker.stubs(:get_worker_data)
+  def stub_film_rating(worker_id, results)
+    MoviesReport::WebSearchWorker
+      .stubs(:get_worker_data)
       .with(worker_id)
-      .returns({ 'state' => state,
-                 'title' => title,
-                 'rating' => rating.to_s })
+      .returns(results)
   end
 
 end
