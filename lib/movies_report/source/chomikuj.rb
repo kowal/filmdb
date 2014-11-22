d86c835b1255ffe588082c06101cdc8943fbf5f8
Marek Kowalcze <marek.kowalcze@gmail.com>
2014-11-22 19:49:19 +0100

    Updated ChomikujSanitizer

--- lib/movies_report/source/chomikuj.rb
+++ lib/movies_report/source/chomikuj.rb
@@ -101,6 +101,7 @@ module MoviesReport
         --oryginalny --orginalny orginalny
         .pl PL IVO(1).avi ivo IVO Ivo 2012.PL dubbing.pl.avi Dubbing
         chomikuj Chomikuj.avi .avi -- .--cam cam.avi ==
+        .rmvb DVDrip.rmvb Rmvb
       }
 
       def self.clean(str)
